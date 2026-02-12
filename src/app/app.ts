8a6505380d35ad65c68cbc841fe921524c163914
Todd <twilliamsark@gmail.com>
2026-02-12 17:07:14 -0600

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `
-    <h1>Hello, {{ title() }}</h1>
-
-    <router-outlet />
-  `,
+  template: ` <router-outlet /> `,
   styles: [],
 })
-export class App {
-  protected readonly title = signal('angularstart-chat');
-}
+export class App {}
---
+++ src/app/home/home.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `<p>Hello, World!</p>`,
+})
+export default class HomeComponent {}
