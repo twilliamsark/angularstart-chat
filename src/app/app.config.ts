d9824cf48a1d43eb020adb99068b1dab6101a268
Todd <twilliamsark@gmail.com>
2026-02-13 07:08:52 -0600

    initial firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,51 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+
+import { initializeApp } from 'firebase/app';
+import {
+  Firestore,
+  initializeFirestore,
+  connectFirestoreEmulator,
+  getFirestore,
+} from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+
+const app = initializeApp(environment.firebase);
+
+export const AUTH = new InjectionToken('Firebase auth', {
+  providedIn: 'root',
+  factory: () => {
+    const auth = getAuth();
+    if (environment.useEmulators) {
+      connectAuthEmulator(auth, 'http://localhost:9099', {
+        disableWarnings: true,
+      });
+    }
+    return auth;
+  },
+});
+
+export const FIRESTORE = new InjectionToken('Firebase firestore', {
+  providedIn: 'root',
+  factory: () => {
+    let firestore: Firestore;
+    if (environment.useEmulators) {
+      firestore = initializeFirestore(app, {});
+      connectFirestoreEmulator(firestore, 'localhost', 8080);
+    } else {
+      firestore = getFirestore();
+    }
+    return firestore;
+  },
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyBKAO4V-Wwwgkb4sZwXvxQkGzwGrS6RgCk',
+    authDomain: 'angularstart-chat-tgw26.firebaseapp.com',
+    projectId: 'angularstart-chat-tgw26',
+    storageBucket: 'angularstart-chat-tgw26.firebasestorage.app',
+    messagingSenderId: '664769442249',
+    appId: '1:664769442249:web:f4bf1be36a67edd6343818',
+  },
+  production: true,
+  useEmulators: false,
+};
