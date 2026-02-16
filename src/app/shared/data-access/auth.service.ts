93b676dcb9521d79d964f955da0d8d1bdd1cf25b
Todd <twilliamsark@gmail.com>
2026-02-16 08:30:06 -0600

    AuthService

---
+++ src/app/shared/data-access/auth.service.ts
@@ -0,0 +1,61 @@
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { from, defer, merge } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {
+  User,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
+import { authState } from 'rxfire/auth';
+import { Credentials } from '../interfaces/credentials';
+import { connect } from 'ngxtension/connect';
+import { AUTH } from '../../app.config';
+
+export type AuthUser = User | null | undefined;
+
+interface AuthState {
+  user: AuthUser;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private auth = inject(AUTH);
+
+  // sources
+  private user$ = authState(this.auth);
+
+  // state
+  private state = signal<AuthState>({
+    user: undefined,
+  });
+
+  // selectors
+  user = computed(() => this.state().user);
+
+  constructor() {
+    const nextState$ = merge(this.user$.pipe(map((user) => ({ user }))));
+
+    connect(this.state).with(nextState$);
+  }
+
+  login(credentials: Credentials) {
+    return from(
+      defer(() => signInWithEmailAndPassword(this.auth, credentials.email, credentials.password)),
+    );
+  }
+
+  logout() {
+    signOut(this.auth);
+  }
+
+  createAccount(credentials: Credentials) {
+    return from(
+      defer(() =>
+        createUserWithEmailAndPassword(this.auth, credentials.email, credentials.password),
+      ),
+    );
+  }
+}
